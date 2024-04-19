1da1ff52f1878fd7d74cbcd205656bd5272e35a5
Ajay Prakash <ajayprakash0320@gmail.com>
2024-04-19 10:49:25 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import LottieView from "lottie-react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { Text, Pressable, StyleSheet, View } from "react-native";
+import * as Haptics from "expo-haptics";
 
 export default function App() {
+  const animation = useRef(null);
+  const [playing, setPlaying] = useState(false);
+  useEffect(() => {
+    if (!playing) {
+      animation.current?.play();
+    } else {
+      animation.current?.pause();
+    }
+  }, [playing]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.animationContainer}>
+      <LottieView
+        ref={animation}
+        style={{
+          width: "100%",
+          height: "80%",
+          backgroundColor: "transparent",
+        }}
+        // Find more Lottie files at https://lottiefiles.com/featured
+        source={require("./assets/gradientBall.json")}
+      />
+      <View style={styles.buttonContainer}>
+        <Pressable
+          style={{
+            ...styles.button,
+          }}
+          onPress={() => {
+            Haptics.impactAsync(playing ? Haptics.ImpactFeedbackStyle.Medium: Haptics.ImpactFeedbackStyle.Heavy);
+            setPlaying(!playing);
+          }}
+        >
+          <Text style={{ color: "white" }}>{`${playing ? "Pause" : "Play"}`}</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  animationContainer: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  buttonContainer: {
+    paddingTop: 20,
+  },
+  button: {
+    width: 200,
+    height: 50,
+    backgroundColor: "black",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.32,
+    shadowRadius: 5.46,
+    elevation: 9,
   },
 });
